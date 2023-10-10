4d808f3ac291f27cf22c466b9fdfa9384f2c0b1d
Arron Alsop <alsop.arron@gmail.com>
2023-10-10 08:42:47 -0700

    'wip'

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,83 +1,126 @@
 import React from 'react'
 
-// Suggested initial states
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 // the index the "B" is at
+// // Suggested initial states
+// const initialMessage = ''
+// const initialEmail = ''
+// const initialSteps = 0
+// const initialIndex = 4 // the index the "B" is at
 
-const initialState = {
-  message: initialMessage,
-  email: initialEmail,
-  index: initialIndex,
-  steps: initialSteps,
-}
+// const initialState = {
+//   message: initialMessage,
+//   email: initialEmail,
+//   index: initialIndex,
+//   steps: initialSteps,
+// }
 
 export default class AppClass extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      bSquareIndex: 4,
+      message: '',
+      steps: 0,
+      email: ''
+    }
+  }
+
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
 
-  getXY = () => {
-    // It it not necessary to have a state to track the coordinates.
-    // It's enough to know what index the "B" is at, to be able to calculate them.
+  getXY = (index) => {
+    const { bSquareIndex } = this.state
+    const y = Math.floor(bSquareIndex / 3) + 1
+    const x = bSquareIndex % 3 + 1
+    return `(${x}, ${y})`
   }
 
-  getXYMessage = () => {
-    // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
-    // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
-    // returns the fully constructed string.
-  }
+  // getXYMessage = () => {
+  //   // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
+  //   // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
+  //   // returns the fully constructed string.
+  // }
 
   reset = () => {
-    // Use this helper to reset all states to their initial values.
-
+    this.setState({
+      steps: 0,
+      bSquareIndex: 4,
+      message: ''
+    })
   }
+
 
   getNextIndex = (direction) => {
-    // This helper takes a direction ("left", "up", etc) and calculates what the next index
-    // of the "B" would be. If the move is impossible because we are at the edge of the grid,
-    // this helper should return the current index unchanged.
+    const { bSquareIndex } = this.state
+    switch (direction) {
+      case 'left':
+        return bSquareIndex % 3 !== 0 ? bSquareIndex - 1 : bSquareIndex;
+      case 'up':
+        return bSquareIndex >= 3 ? bSquareIndex - 3 : bSquareIndex;
+      case 'right':
+        return bSquareIndex % 3 !== 2 ? bSquareIndex + 1 : bSquareIndex;
+      case 'down':
+        return bSquareIndex <= 5 ? bSquareIndex + 3 : bSquareIndex;
+      default:
+        return bSquareIndex;
+    }
   }
 
-  move = (evt) => {
-    // This event handler can use the helper above to obtain a new index for the "B",
-    // and change any states accordingly.
+  move = (direction) => {
+    const { bSquareIndex, steps } = this.state
+    const newIndex = this.getNextIndex(direction);
+    if (newIndex !== this.state.bSquareIndex) {
+      this.setState(prevState => ({
+        steps: prevState.steps + 1,
+        bSquareIndex: newIndex,
+        message: ''
+      }))
+    } else {
+      this.setState({ message: `You can't go ${direction}` })
+    }
   }
 
-  onChange = (evt) => {
-    // You will need this to update the value of the input.
-  }
+  // onChange = (evt) => {
+  //   // You will need this to update the value of the input.
+  // }
 
   onSubmit = (evt) => {
-    // Use a POST request to send a payload to the server.
+    evt.preventDefault()
+  const { bSquareIndex, steps, email } = this.state
+  const coordinates = this.getXY(bSquareIndex)
+    
+    const payload = {
+      x: coordinates.x,
+      y: coordinates.y,
+      steps: steps,
+      email: email
+    }
   }
-
   render() {
     const { className } = this.props
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">Coordinates {this.getXY()}</h3>
+          <h3 id="steps">You moved {steps} times</h3>
         </div>
         <div id="grid">
           {
             [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-              <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-                {idx === 4 ? 'B' : null}
+              <div key={idx} className={`square${idx === this.state.bSquareIndex ? ' active' : ''}`}>
+                {idx === this.state.bSquareIndex ? 'B' : null}
               </div>
             ))
           }
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{message}</h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button id="left" onClick={()=> this.move('left')}>LEFT</button>
+          <button id="up" onClick={()=> this.move('up')}>UP</button>
+          <button id="right" onClick={()=> this.move('right')}>RIGHT</button>
+          <button id="down" onClick={()=> this.move('down')}>DOWN</button>
+          <button id="reset" onClick={()=> this.move('reset')}>reset</button>
         </div>
         <form>
           <input id="email" type="email" placeholder="type email"></input>
@@ -86,4 +129,4 @@ export default class AppClass extends React.Component {
       </div>
     )
   }
-}
+  }
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -88,43 +88,6 @@ export default function AppFunctional(props) {
     })
   }
 
-
-
-
-  // function getXY() {
-  //   // It it not necessary to have a state to track the coordinates.
-  //   // It's enough to know what index the "B" is at, to be able to calculate them.
-  // }
-
-  // function getXYMessage() {
-  //   // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
-  //   // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
-  //   // returns the fully constructed string.
-  // }
-
-  // function reset() {
-  //   // Use this helper to reset all states to their initial values.
-  // }
-
-  // function getNextIndex(direction) {
-  //   // This helper takes a direction ("left", "up", etc) and calculates what the next index
-  //   // of the "B" would be. If the move is impossible because we are at the edge of the grid,
-  //   // this helper should return the current index unchanged.
-  // }
-
-  // function move(evt) {
-  //   // This event handler can use the helper above to obtain a new index for the "B",
-  //   // and change any states accordingly.
-  // }
-
-  // function onChange(evt) {
-  //   // You will need this to update the value of the input.
-  // }
-
-  // function onSubmit(evt) {
-  //   // Use a POST request to send a payload to the server.
-  // }
-
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
