990811dc2f0fe5027156e986f28c9945c5c766f9
Arron Alsop <alsop.arron@gmail.com>
2023-10-10 20:07:34 -0700

    'wip'

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,22 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+import React from 'react'
+import { render, fireEvent, waitFor } from '@testing-library/react'
+import AppFunctional from './AppFunctional'
+
+test('renders without errors', () => {
+  render(<AppFunctional />)
 })
+
+test('typing in the input field changes its value', async () => {
+  const { getByPlaceholderText } = render(<AppFunctional />)
+  const emailInput = getByPlaceholderText('type email')
+  fireEvent.change(emailInput, { target: { value: 'testing@out.com'} })
+  
+  await waitFor(() => {
+    expect(emailInput.value).toBe('testing@out.com')
+  })
+})
+
+
+// test('sanity', () => {
+//   expect(true).toBe(false)
+// })
